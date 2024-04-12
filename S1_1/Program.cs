56b4cd58fe5b90a8924f0ae4f68eff2a25e63530
Max Denisenko <maksim.denisenko.20@mail.ru>
2024-04-12 12:47:13 +0300

    Добавьте файлы проекта.

---
+++ S1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace S1_1
+{
+    class Program
+    {
+     
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i);
+                a++;
+            }
+        }
+    }
+}
